c9037696abdf078ab3d4f20eb88a5304129818af
Serenity <d.palbanese@gmail.com>
2022-09-15 20:06:15 +0200

    change

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -7,13 +7,17 @@ int main() {
     Util::log::Init("Matching Engine");
 
     Matching::MatchingEngine engine;
+
+    Data::Symbol symbol{1, "AAPL"};
+    engine.AddSymbol(symbol);
+
     Data::Order order(1,
                       1,
-                      TradingEngine::Data::OrderType::MARKET,
+                      TradingEngine::Data::OrderType::LIMIT,
                       TradingEngine::Data::OrderSide::BUY,
                       TradingEngine::Data::OrderLifetime::GFD,
                       10,
                       10);
 
-    engine.AddOrder(std::move(order));
+    engine.AddOrder(order);
 }
