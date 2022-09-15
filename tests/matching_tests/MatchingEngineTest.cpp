5e4a9d3bf2c8a83ddfac9c6a2d87215664689381
Serenity <d.palbanese@gmail.com>
2022-09-15 21:21:58 +0200

    Added IOC SELL test

--- tests/matching_tests/MatchingEngineTest.cpp
+++ tests/matching_tests/MatchingEngineTest.cpp
@@ -58,9 +58,59 @@ TEST_F(MatchingEngineTest, TestIOCBuy) {
             TradingEngine::Data::OrderType::IOC,
             TradingEngine::Data::OrderSide::BUY,
             TradingEngine::Data::OrderLifetime::GFD,
-            30,
+            50,
             5
     };
     m_engine.AddOrder(order);
     EXPECT_EQ(ob->BestAsk()->Price, 35);
+}
+
+TEST_F(MatchingEngineTest, TestIOCSell) {
+    TradingEngine::Data::Order order {
+            11,
+            1,
+            TradingEngine::Data::OrderType::IOC,
+            TradingEngine::Data::OrderSide::SELL,
+            TradingEngine::Data::OrderLifetime::GFD,
+            25,
+            5
+    };
+    m_engine.AddOrder(order);
+    auto ob = m_engine.OrderBook(1);
+    EXPECT_EQ(ob->BestBid()->TotalVolume, 5);
+
+    auto& bids = ob->Bids();
+    auto totalVol = [](auto& s) -> uint64_t {
+        uint64_t vol = 0;
+        for (auto& lvl:s) {
+            vol += lvl.TotalVolume;
+        }
+        return vol;
+    };
+    auto volBefore = totalVol(bids);
+    order = {
+            12,
+            1,
+            TradingEngine::Data::OrderType::IOC,
+            TradingEngine::Data::OrderSide::SELL,
+            TradingEngine::Data::OrderLifetime::GFD,
+            30,
+            5
+    };
+    m_engine.AddOrder(order);
+    auto volAfter = totalVol(bids);
+
+    EXPECT_EQ(volBefore, volAfter);
+
+    order= {
+            13,
+            1,
+            TradingEngine::Data::OrderType::IOC,
+            TradingEngine::Data::OrderSide::SELL,
+            TradingEngine::Data::OrderLifetime::GFD,
+            10,
+            5
+    };
+    m_engine.AddOrder(order);
+    EXPECT_EQ(ob->BestBid()->Price, 20);
 }
