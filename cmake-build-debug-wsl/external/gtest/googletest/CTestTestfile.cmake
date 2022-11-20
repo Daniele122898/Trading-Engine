b6c8df906622fc86a7e12ebe3ee670ca5df79b9a
Daniele122898 <d.palbanese@gmail.com>
2022-11-20 17:01:05 +0100

    remove cmake build folder

--- cmake-build-debug-wsl/external/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniele/dev/cpp/Trading-Engine/external/gtest/googletest
-# Build directory: /home/daniele/dev/cpp/Trading-Engine/cmake-build-debug-wsl/external/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug-wsl/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniele/dev/cpp/Trading-Engine/tests
-# Build directory: /home/daniele/dev/cpp/Trading-Engine/cmake-build-debug-wsl/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
