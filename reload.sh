39efed7b8842133d27e5286e012039711a3df70e
Daniele122898 <d.palbanese@gmail.com>
2022-12-22 23:18:00 +0100

    Added bash files to build

---
+++ reload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir cmake-build-debug-wsl
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang-14 '-DCMAKE_CXX_COMPILER=clang++-14' -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S ./ -B ./cmake-build-debug-wsl
